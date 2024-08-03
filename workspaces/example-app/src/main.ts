d3492367c48fed1b3a4803a7484f3528c2137df0
jkh675 <rluo2018@gmail.com>
2024-08-03 14:23:27 +0800

    Implement basic coposite system

--- workspaces/example-app/src/main.ts
+++ workspaces/example-app/src/main.ts
@@ -1,5 +1,45 @@
-import { Hello } from "di-ui.js";
+/* eslint-disable*/
+import * as Di from "di-ui.js";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  ${Hello()}
-`;
+class DivContainer extends Di.Components.DrawableComponent {
+	constructor(name: string = "DivContainer") {
+		super();
+		this.ComponentName = name;
+	}
+
+	override Render(): Element {
+		super.Render();
+		this.CurrentElement!.id = this.ComponentName;
+		this.CurrentElement!.prepend(
+			document.createTextNode(this.ComponentName)
+		);
+		return this.CurrentElement!;
+	}
+}
+
+const ROOT = new DivContainer();
+
+const HELLO = new DivContainer("Hello");
+
+HELLO.SetAttribute("style", "color: blue;")
+	.Add(
+		new DivContainer("World")
+			.SetAttribute("style", "color: red;")
+			.SetAttribute("id", "world")
+	)
+	.Add(new DivContainer("!!!"));
+
+ROOT.Add(HELLO);
+
+const APP = new Di.App.SpaAppEntry(ROOT);
+
+const FRAMEWORK = new Di.Framework.Framework(
+	APP,
+	document.getElementById("app")!
+);
+
+setTimeout(() => {
+	ROOT.Dispose();
+}, 10000);
+
+FRAMEWORK.Start();
