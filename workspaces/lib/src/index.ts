bab0712655a31596047711735aa70814dc29d3af
jkh675 <rluo2018@gmail.com>
2024-08-03 19:03:17 +0800

    Add custom type

--- workspaces/lib/src/index.ts
+++ workspaces/lib/src/index.ts
@@ -1,8 +1,5 @@
-export function Hello(): string {
-	return "This is the start of a new era";
-}
-
 export * from "./App";
 export * from "./Framework";
 export * from "./Composite";
 export * from "./Components";
+export * from "./types"
