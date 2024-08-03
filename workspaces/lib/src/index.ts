54a6d7c3c73fbd5d05bccf5b235c32d12f3102dc
jkh675 <rluo2018@gmail.com>
2024-08-03 21:46:03 +0800

    Fix the default export

--- workspaces/lib/src/index.ts
+++ workspaces/lib/src/index.ts
@@ -1,5 +1,15 @@
-export * from "./App";
-export * from "./Framework";
-export * from "./Composite";
-export * from "./Components";
-export * from "./types";
+import { App } from "./App";
+import { DependencyInjection, Framework, MetadataKeys } from "./Framework";
+import { Composite } from "./Composite";
+import { Components } from "./Components";
+import { HTMLTags } from "./types";
+
+export default {
+	App,
+	DependencyInjection,
+	Framework,
+	MetadataKeys,
+	Composite,
+	Components,
+};
+export type { HTMLTags };
