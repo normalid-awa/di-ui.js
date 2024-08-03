a40325e15d3ce7e5e8449846d0910c8e9a7692a7
normalid <52519933+normalid-awa@users.noreply.github.com>
2024-08-03 19:36:45 +0800

    Revert "Implement basic coposite system"
    This reverts commit f1d1ce92aa5687706612c81b83ef4c96388fa8a5.

--- workspaces/lib/src/index.ts
+++ workspaces/lib/src/index.ts
@@ -1,8 +1,3 @@
 export function Hello(): string {
 	return "This is the start of a new era";
 }
-
-export * from "./App";
-export * from "./Framework";
-export * from "./Composite";
-export * from "./Components";
