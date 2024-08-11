7e55f31b41317ed7525648dafe80129a4bf34f19
normalid-awa <rluo2018@gmail.com>
2024-08-11 18:01:41 +0800

    Improve unit test

--- workspaces/test/src/Bindable.test.ts
+++ workspaces/test/src/Bindable.test.ts
@@ -37,18 +37,18 @@ test("Test Bindable Value Change Event", () => {
 test("Test Bindable Default Value Change Event", () => {
 	const bind = new Bindable<number>(0);
 
-	const on_default_val_changed = vi.fn((v: IValueChangedEvent<number>) => v);
+	const on_default_val_changed = vi.fn();
 
 	bind.OnDefaultValueChanged(on_default_val_changed, true);
 
-	expect(on_default_val_changed).toHaveReturnedWith({
+	expect(on_default_val_changed).nthCalledWith(1, {
 		OldValue: 0,
 		NewValue: 0,
 	} satisfies IValueChangedEvent<number>);
 
 	bind.DefaultValue = 10;
 
-	expect(on_default_val_changed).toHaveReturnedWith({
+	expect(on_default_val_changed).nthCalledWith(2, {
 		OldValue: 0,
 		NewValue: 10,
 	} satisfies IValueChangedEvent<number>);
@@ -74,7 +74,7 @@ test("Test Bindable Bind To Others", () => {
 	expect(other_bind.Value).eq(10);
 
 	root_bind.Value = -10;
-	other_bind.Value = 100
+	other_bind.Value = 100;
 
 	expect(root_bind.Value).eq(-10);
 	expect(other_bind.Value).eq(100);
@@ -84,16 +84,37 @@ test("Test Editable Binding", () => {
 	const root_bind = new Bindable<number>(0);
 	const other_bind = root_bind.GetEditableCopy();
 
+	const root_val_change = vi.fn();
+	const other_val_change = vi.fn();
+	root_bind.OnValueChanged(root_val_change);
+	other_bind.OnValueChanged(other_val_change);
+
 	expect(root_bind.Value).eq(0);
 	expect(other_bind.Value).eq(0);
 
 	root_bind.Value = 10;
 
+	expect(root_val_change).nthCalledWith(1, {
+		OldValue: 0,
+		NewValue: 10,
+	} satisfies IValueChangedEvent<number>);
+	expect(other_val_change).nthCalledWith(1, {
+		OldValue: 0,
+		NewValue: 10,
+	} satisfies IValueChangedEvent<number>);
 	expect(root_bind.Value).eq(10);
 	expect(other_bind.Value).eq(10);
 
 	other_bind.Value = 100;
 
+	expect(root_val_change).nthCalledWith(2, {
+		OldValue: 10,
+		NewValue: 100,
+	} satisfies IValueChangedEvent<number>);
+	expect(other_val_change).nthCalledWith(2, {
+		OldValue: 10,
+		NewValue: 100,
+	} satisfies IValueChangedEvent<number>);
 	expect(root_bind.Value).eq(100);
 	expect(other_bind.Value).eq(100);
 
@@ -103,8 +124,8 @@ test("Test Editable Binding", () => {
 	expect(other_bind.Value).eq(100);
 
 	root_bind.Value = -10;
-	other_bind.Value = 1001
+	other_bind.Value = 1001;
 
 	expect(root_bind.Value).eq(-10);
 	expect(other_bind.Value).eq(1001);
-})
+});
